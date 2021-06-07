5d8b4f3170f4add63a0a6fbaba4bd777ccda4c7f
BeatrizWF <79949234+BeatrizWF@users.noreply.github.com>
2021-06-06 22:43:14 -0300

    Delete RelogioPooJava directory

--- RelogioPooJava/src/relogiopoojava/RelogioJava.java
+++
@@ -1,8 +0,0 @@
-package relogiopoojava;
-public class RelogioJava{
-    public static void main(String[] args) {
-    Hora h1 = new Hora();
-    h1.addHorario(23,2,58,4);
-    h1.getHorarioAmPm();
- }   
-}
