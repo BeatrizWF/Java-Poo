3087dfe2eb91de9ffbaff34b47af6c0d5c43a3ca
BeatrizWF <79949234+BeatrizWF@users.noreply.github.com>
2021-09-10 12:52:54 -0300

    Update PessoaDAOImpl.java

--- app/app/src/App/repositorio/PessoaDAOImpl.java
+++ app/app/src/App/repositorio/PessoaDAOImpl.java
@@ -2,7 +2,6 @@
 package App.repositorio;
 import App.entidade.Pessoa;
 import java.util.HashMap;
-//import java.util.List;
 import java.util.Map;
 
 public class PessoaDAOImpl implements PessoaDAO {
